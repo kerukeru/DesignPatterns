1f5c19280d5385b82090be06aad6d1f9a0dedc71
kerukeru <keru@protonmail.com>
2019-08-21 15:05:23 +0200

    singleton wzorzec

---
+++ src/Singleton/GameEngine.java
@@ -0,0 +1,45 @@
+package Singleton;
+
+import java.io.Serializable;
+
+public class GameEngine implements Serializable {
+
+    //private static final long serialVersionUID = 6085237886067446069L;
+
+    private int hp = 100;
+    private String characterName="";
+
+    private static GameEngine instance = new GameEngine();
+
+
+    private GameEngine() {
+
+    }
+
+    public void run(){
+        while(true){
+
+
+
+        }
+    }
+
+    public static GameEngine getInstance(){
+        if (instance==null){
+
+            synchronized(GameEngine.class) {
+
+                if (instance==null) {
+                    instance = new GameEngine();
+                }
+            }
+        }
+
+        return instance;
+    }
+
+
+    protected Object readResolve(){
+        return getInstance();
+    }
+}
---
+++ src/Singleton/Main.java
@@ -0,0 +1,17 @@
+package Singleton;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        GameEngine ge = GameEngine.getInstance();
+
+        GameEngine ge2 = GameEngine.getInstance();
+
+
+        System.out.println(ge==ge2);
+
+
+
+    }
+}
